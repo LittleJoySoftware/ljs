c99bf5cca2d25a0975b959aea75abda05fd285ba
Joshua Moody <joshuajmoody@gmail.com>
2014-01-27 20:23:54 +0100

    correctly capturing the RETVAL of iOS tests

--- run-tests.sh
+++ run-tests.sh
@@ -17,6 +17,7 @@ cd ..
 
 cd Tests-iOS
 make clean && WRITE_JUNIT_XML=YES make test
+RETVAL=`echo $?`
 
 if [ $RETVAL = 0 ]; then
   echo "NOTE: iOS unit tests passed"
@@ -24,7 +25,6 @@ else
   echo "FAIL: iOS unit tests failed"
   exit $RETVAL
 fi
-
 
 echo "NOTE: *********************************"
 echo "NOTE: LJS MacOS unit tests passed"
